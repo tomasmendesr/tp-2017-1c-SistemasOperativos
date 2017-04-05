3b16986851ab7cf8308e364c1a66b51ed62487ba
Matias <matias.martello@gmail.com>
2017-04-04 21:08:12 -0300

    Conexion consola kernel

--- consola/src/consola.c
+++ consola/src/consola.c
@@ -8,10 +8,44 @@
  ============================================================================
  */
 
-#include <stdio.h>
-#include <stdlib.h>
 
-int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
-	return EXIT_SUCCESS;
+#define MAX_COMMAND_SIZE 256
+#define IniciarProceso "iniciarProceso"
+#define IP "127.0.0.1"
+#define PUERTO "8080"
+
+#include "consola.h"
+
+int main(){
+   char *comando = malloc(MAX_COMMAND_SIZE);
+   int socket_cliente;
+   fgets(comando,MAX_COMMAND_SIZE,stdin);
+
+   char **comandos = string_split(comando, ".");
+
+   if (comandos)
+   {
+	   //Iniciar Proceso
+	   if(strcmp(comandos[0],IniciarProceso)){
+    	   printf("Su proceso se inicializo");
+    	   socket_cliente = createClient(IP, PUERTO);
+    	   if(socket_cliente){
+    		   printf("Cliente creado satisfactoriamente.\n");
+    	   }
+    	   enviar_paquete_vacio(HANDSHAKE_PROGRAMA,socket_cliente);
+    	   int operacion = 0;
+    	   void* paquete_vacio;
+
+    	   recibir_paquete(socket_cliente, &paquete_vacio, &operacion);
+
+    	   	if(operacion == HANDSHAKE_KERNEL){
+    	   		printf("Conexion con Kernel establecida! :D \n");
+    	   		printf("Se procede a mandar el archivo: ",comando[1]);
+    	   	} else {
+    	   		printf("El Kernel no devolvio handshake :( \n");
+    	   	}
+       }
+   }
+
+   return EXIT_SUCCESS;
 }
---
+++ consola/src/consola.h
@@ -0,0 +1,17 @@
+/*
+ * consola.h
+ *
+ *  Created on: 4/4/2017
+ *      Author: utnso
+ */
+
+#ifndef CONSOLA_H_
+#define CONSOLA_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <commons/string.h>
+#include <commons/sockets.h>
+
+#endif /* CONSOLA_H_ */
