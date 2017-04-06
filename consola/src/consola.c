8d3493f14eb11c629446f5a4bf927923088ea0c9
utnso <fede21perez@hotmail.com>
2017-04-06 15:08:03 -0300

    Consola: muevo las funciones de main a sus respectivos lugares
    --La creacion de config, a una nueva funcion
    --Iniciar un proceso nuevo al hilo de interfaz

--- consola/src/consola.c
+++ consola/src/consola.c
@@ -15,49 +15,11 @@ int main(int argc, char** argv){
 
 	crearLog();
 
-	char* pathConfig=string_new();
-	if(!argv[1])string_append(&pathConfig,argv[1]);
-		else string_append(&pathConfig,configuracionConsola);
+	crearConfig(argv[1]);
 
-	if(verificarExistenciaDeArchivo(pathConfig)){
-		config = levantarConfiguracionConsola(pathConfig);
-	}else{
-		log_info(logger,"No Pudo levantarse el archivo de configuracion");
-		return EXIT_FAILURE;
-	}
+	levantarInterfaz();
 
-   char *comando = malloc(MAX_COMMAND_SIZE);
-   int socket_cliente;
-   fgets(comando,MAX_COMMAND_SIZE,stdin);
-
-   char **comandos = string_split(comando, ".");
-
-   if (comandos)
-   {
-	   //Iniciar Proceso
-	   if(!strcmp(comandos[0],IniciarProceso)){
-		   verificarExistenciaDeArchivo(comandos[1]);
-    	   printf("Su proceso se inicializo");
-    	   socket_cliente = createClient(config->ip_Kernel, config->puerto_Kernel);
-    	   if(socket_cliente){
-    		   printf("Cliente creado satisfactoriamente.\n");
-    	   }
-    	   enviar_paquete_vacio(HANDSHAKE_PROGRAMA,socket_cliente);
-    	   int operacion = 0;
-    	   void* paquete_vacio;
-
-    	   recibir_paquete(socket_cliente, &paquete_vacio, &operacion);
-
-    	   	if(operacion == HANDSHAKE_KERNEL){
-    	   		printf("Conexion con Kernel establecida! :D \n");
-    	   		printf("Se procede a mandar el archivo: ",comando[1]);
-    	   	} else {
-    	   		printf("El Kernel no devolvio handshake :( \n");
-    	   	}
-       }
-   }
-
-   return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
 
 
--- consola/src/funcionesConsola.c
+++ consola/src/funcionesConsola.c
@@ -1,30 +1,48 @@
-
-
 #include "funcionesConsola.h"
+
+void crearConfig(char* configPath) {
+	char* pathConfig = string_new();
+	if (configPath)
+		string_append(&pathConfig, configPath);
+	else
+		string_append(&pathConfig, configuracionConsola);
+
+	if (verificarExistenciaDeArchivo(pathConfig)) {
+		config = levantarConfiguracionConsola(pathConfig);
+	} else {
+		log_info(logger, "No Pudo levantarse el archivo de configuracion");
+		exit(EXIT_FAILURE);
+	}
+}
 
 t_config_consola* levantarConfiguracionConsola(char * archivo) {
 
-        t_config_consola* config = malloc(sizeof(t_config_consola));
-        t_config* configConsola;
+	t_config_consola* config = malloc(sizeof(t_config_consola));
+	t_config* configConsola;
 
-        verificarExistenciaDeArchivo(archivo);
-        configConsola = config_create(archivo);
+	verificarExistenciaDeArchivo(archivo);
+	configConsola = config_create(archivo);
 
-        config->ip_Kernel = malloc(strlen(config_get_string_value(configConsola, "IP_KERNEL"))+1);
-        strcpy(config->ip_Kernel,config_get_string_value(configConsola, "IP_KERNEL"));
+	config->ip_Kernel = malloc(
+			strlen(config_get_string_value(configConsola, "IP_KERNEL")) + 1);
+	strcpy(config->ip_Kernel,
+			config_get_string_value(configConsola, "IP_KERNEL"));
 
-        config->puerto_Kernel = malloc(strlen(config_get_string_value(configConsola, "PUERTO_KERNEL"))+1);
-        strcpy(config->puerto_Kernel,config_get_string_value(configConsola, "PUERTO_KERNEL"));
+	config->puerto_Kernel = malloc(
+			strlen(config_get_string_value(configConsola, "PUERTO_KERNEL"))
+					+ 1);
+	strcpy(config->puerto_Kernel,
+			config_get_string_value(configConsola, "PUERTO_KERNEL"));
 
-        config_destroy(configConsola);
+	config_destroy(configConsola);
 
-        return config;
+	return config;
 }
 
 //funciones interfaz
-void levantarInterfaz(){
+void levantarInterfaz() {
 	//creo los comandos y el parametro
-	comando* comandos = malloc(sizeof(comando)*4);
+	comando* comandos = malloc(sizeof(comando) * 4);
 
 	strcpy(comandos[0].comando, "start");
 	comandos[0].funcion = iniciarPrograma;
@@ -45,28 +63,53 @@ void levantarInterfaz(){
 	pthread_attr_init(&atributos);
 	pthread_attr_setdetachstate(&atributos, PTHREAD_CREATE_DETACHED);
 
-	pthread_create(&threadInterfaz, &atributos, (void*)interface, params);
+	pthread_create(&threadInterfaz, &atributos, (void*) interface, params);
 
 	return;
 }
-void iniciarPrograma(char* comando, char* param){
-        printf("iniciarPrograma\n");
+void iniciarPrograma(char* comando, char* param) {
+
+	int socket_cliente;
+
+	verificarExistenciaDeArchivo(param);
+	printf("Su proceso se inicializo");
+	socket_cliente = createClient(config->ip_Kernel, config->puerto_Kernel);
+	if (socket_cliente) {
+		printf("Cliente creado satisfactoriamente.\n");
+	}
+	enviar_paquete_vacio(HANDSHAKE_PROGRAMA, socket_cliente);
+	int operacion = 0;
+	void* paquete_vacio;
+
+	recibir_paquete(socket_cliente, &paquete_vacio, &operacion);
+
+	if (operacion == HANDSHAKE_KERNEL) {
+		printf("Conexion con Kernel establecida! :D \n");
+		printf("Se procede a mandar el archivo: ", comando[1]);
+	} else {
+		printf("El Kernel no devolvio handshake :( \n");
+	}
+	printf("iniciarPrograma\n");
 }
-void finalizarPrograma(char* comando, char* param){
-        printf("finalizarPrograma\n");
+void finalizarPrograma(char* comando, char* param) {
+	printf("finalizarPrograma\n");
 }
-void desconectarConsola(char* comando, char* param){
-        printf("desconectarConsola\n");
+void desconectarConsola(char* comando, char* param) {
+	printf("desconectarConsola\n");
 }
-void limpiarMensajes(char* comando, char* param){
-        printf("limpiarMensajes");
+void limpiarMensajes(char* comando, char* param) {
+	printf("limpiarMensajes");
 }
 
 int crearLog() {
-        logger = log_create(getenv("/home/utnso/TPOperativos/tp-2017-1c-Dirty-Cow/consola/logConsola"), "consola", 1, 0);
-        if (logger) {
-                return 1;
-        } else {
-                return 0;
-        }
+	logger =
+			log_create(
+					getenv(
+							"/home/utnso/TPOperativos/tp-2017-1c-Dirty-Cow/consola/logConsola"),
+					"consola", 1, 0);
+	if (logger) {
+		return 1;
+	} else {
+		return 0;
+	}
 }
