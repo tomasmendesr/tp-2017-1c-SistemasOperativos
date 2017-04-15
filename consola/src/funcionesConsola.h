4d783137c9ebe4abd740aa05e968a080728ac876
Matias <matias.martello@gmail.com>
2017-04-15 14:16:22 -0300

    Cambios consola

--- consola/src/funcionesConsola.h
+++ consola/src/funcionesConsola.h
@@ -24,7 +24,7 @@
 
 #define MAX_COMMAND_SIZE 256
 #define IniciarProceso "iniciarProceso"
-#define configuracionConsola "confConsola.init"
+#define configuracionConsola "../confConsola.init"
 
 typedef struct{
         char* ip_Kernel;
