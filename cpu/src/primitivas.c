eb919bb2551cc8b84efaccf93043abe12cc2a261
tomasmendesr <tomasmendesr@gmail.com>
2017-05-08 00:55:21 -0300

    Arreglo un detalle

--- cpu/src/primitivas.c
+++ cpu/src/primitivas.c
@@ -306,12 +306,15 @@ t_puntero obtenerPosicionVariable(t_nombre_variable identificador_variable){
 
 	if(!esArgumento(identificador_variable)){ // es una variable
 		t_var* var_local;
+		bool notFound = true;
 		for(i=0; i < list_size(contexto->variables); i++){
 			var_local = list_get(contexto->variables, i);
-			if(var_local->id == identificador_variable)
+			if(var_local->id == identificador_variable){
+				notFound = false;
 				break;
+			}
 		}
-		if(list_size(contexto->variables) < i){
+		if(notFound){
 			log_error(logger, "No se encontro la variable %c en el stack", identificador_variable);
 			return EXIT_FAILURE;
 		}
