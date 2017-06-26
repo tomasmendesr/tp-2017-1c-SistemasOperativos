a38d8f079c475a1f170676fbd0ba8da221f17087
utnso <fede21perez@hotmail.com>
2017-06-26 14:37:51 -0300

    Fix envio archivo
    --Agrega \0 al final

--- consola/src/funcionesConsola.c
+++ consola/src/funcionesConsola.c
@@ -60,28 +60,29 @@ int enviarArchivo(int kernel_fd, char* path){
  		return -1;
  	}
 
- 	FILE* file;
+ 	FILE* sourceFile;
  	int file_fd, file_size;
  	struct stat stats;
 
  	//Abro el archivo y le saco los stats
- 	file = fopen(path, "r");
+ 	sourceFile = fopen(path, "r");
  	//esto nunca deberia fallar porque ya esta verificado, pero por las dudas
- 	if(file == NULL){
+ 	if(sourceFile == NULL){
  		log_error(logger, "No pudo abrir el archivo");
  		return -1;
  	}
- 	file_fd = fileno(file);
+ 	file_fd = fileno(sourceFile);
 
  	fstat(file_fd, &stats);
  	file_size = stats.st_size;
  	header_t header;
- 	char* buffer = malloc(file_size + sizeof(header_t)+1);
+ 	char* buffer = malloc(file_size + 1 + sizeof(header_t));
+ 	memset(buffer,'@',file_size + 1 + sizeof(header_t));
  	int offset = 0;
 
  	if(buffer == NULL){
  		log_error(logger, "No se pudo reservar memoria para enviar archivo");
- 		fclose(file);
+ 		fclose(sourceFile);
  		return -1;
  	}
 
@@ -90,15 +91,18 @@ int enviarArchivo(int kernel_fd, char* path){
  	memcpy(buffer, &(header.type),sizeof(header.type)); offset+=sizeof(header.type);
  	memcpy(buffer + offset, &(header.length),sizeof(header.length)); offset+=sizeof(header.length);
 
- 	if(fread(buffer + offset,file_size,1,file) < 1){
+ 	if(fread(buffer + offset,file_size,1,sourceFile) < 1){
  		log_error(logger, "No es posible leer el archivo");
  		free(buffer);
- 		fclose(file);
+ 		fclose(sourceFile);
  		return -1;
  	}
 
+ 	//Agrego \0
+ 	(buffer + offset)[file_size] = '\0';
+
  	printf("Archivo a enviar:\n%s\n",buffer + offset);
- 	printf("Info header. type: %d length: %d\n",ENVIO_CODIGO,file_size);
+ 	printf("Info header. type: %d length: %d\n",ENVIO_CODIGO,file_size + 1);
 
  	FILE* dumpFile = fopen("Dump","w");
 
@@ -112,11 +116,11 @@ int enviarArchivo(int kernel_fd, char* path){
  	if(sendAll(kernel_fd, buffer, file_size + sizeof(header_t) + 1, 0) <= 0){
  		log_error(logger, "Error al enviar archivo");
  		free(buffer);
- 		fclose(file);
+ 		fclose(sourceFile);
  		return -1;
  	}
  	free(buffer);
- 	fclose(file);
+ 	fclose(sourceFile);
  	return 0;
 }
 
